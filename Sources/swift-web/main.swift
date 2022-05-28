52f3c3106929f90de077383f7c0dde734fb3760f
kunihiro-dev <kunihiro-dev@users.noreply.github.com>
2022-05-28 10:08:12 +0900

    fixed recieve response

--- Sources/swift-web/main.swift
+++ Sources/swift-web/main.swift
@@ -1,36 +1,43 @@
 import Foundation
 import FoundationNetworking
 
-print("Start server!!!")
 
-// create url session
-print("Creating URL session...")
-let config = URLSessionConfiguration.default
-let session = URLSession(configuration: config)
+func urlRequest(url: String) {
+    print("Start server!!!")
 
-// making url
-print("Making URL...")
-let urlcomponents = URLComponents(string: "http://localhost:3030")
-let url = urlcomponents?.url
+    // create url session
+    print("Creating URL session...")
+    let config = URLSessionConfiguration.default
+    let session = URLSession(configuration: config)
 
-let task = session.dataTask(with: url!) { data, response, error in
-    print("Error check...")
-    if let error = error {
-        print(error.localizedDescription)
-        return
+    // making url
+    print("Making URL...")
+    let urlcomponents = URLComponents(string: url)
+    let url = urlcomponents?.url
+
+    let semaphore = DispatchSemaphore(value: 0)
+    let task = session.dataTask(with: url!) { data, response, error in
+        print("Error check...")
+        if let error = error {
+            print(error.localizedDescription)
+            return
+        }
+
+        print("Data check...")
+        guard let data = data, let response = response as? HTTPURLResponse else {
+            print("No data")
+            return
+        }
+
+        print("Response check...")
+        let responseContent = String(data: data, encoding: .utf8)!
+        print(response.statusCode)
+        print(responseContent)
+        semaphore.signal()
     }
-
-    print("Data check...")
-    guard let data = data, let response = response as? HTTPURLResponse else {
-        print("No data")
-        return
-    }
-
-    print("Response check...")
-    let responseContent = String(data: data, encoding: .utf8)!
-    print(response.statusCode)
-    print(responseContent)
+    print("Server run...")
+    task.resume()
+    semaphore.wait()
 }
 
-print("Server run...")
-task.resume()
+urlRequest(url: "http://localhost:3030")
